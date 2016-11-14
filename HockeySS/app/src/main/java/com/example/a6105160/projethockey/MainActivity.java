58ee7a964948ec66a886e20b855021462f48de4f
Caya <6105160@cegepdrummond.ca>
2016-11-14 14:31:34 -0500

    mainactivity

--- HockeySS/app/src/main/java/com/example/a6105160/projethockey/MainActivity.java
+++ HockeySS/app/src/main/java/com/example/a6105160/projethockey/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.a6105160.projethockey;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    Button button4 = (Button) findViewById(R.id.button4);
+        button4.setOnClickListener(new OnClickListener() {
+
+        @Override
+        public void onClick(View v) {
+            // TODO Auto-generated method stub
+            finish();
+            System.exit(0);
+        }
+    });
     }
 }
