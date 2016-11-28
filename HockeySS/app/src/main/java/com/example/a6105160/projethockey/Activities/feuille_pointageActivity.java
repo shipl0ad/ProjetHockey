0a84975cef7186987234d579330a279cd5689912
Caya <6105160@cegepdrummond.ca>
2016-11-28 15:52:17 -0500

    minsubuttonmarde

--- HockeySS/app/src/main/java/com/example/a6105160/projethockey/Activities/feuille_pointageActivity.java
+++ HockeySS/app/src/main/java/com/example/a6105160/projethockey/Activities/feuille_pointageActivity.java
@@ -2,27 +2,24 @@ package com.example.a6105160.projethockey.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.TypedValue;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.example.a6105160.projethockey.R;
 
 public class feuille_pointageActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.feuille_de_pointage);
 
-        Button boutonRetour = (Button) findViewById(R.id.button38);
-        boutonRetour.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                finish();
-            }
-        });
 
-    }
 }
+}
+
 
