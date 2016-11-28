09b9ef2b987262083c9f74e86527ac7d29103903
Caya <6105160@cegepdrummond.ca>
2016-11-28 15:00:21 -0500

    enlever ligue

--- HockeySS/app/src/main/java/com/example/a6105160/projethockey/Activities/feuille_pointageActivity.java
+++ HockeySS/app/src/main/java/com/example/a6105160/projethockey/Activities/feuille_pointageActivity.java
@@ -2,6 +2,9 @@ package com.example.a6105160.projethockey.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.a6105160.projethockey.R;
 
@@ -11,6 +14,14 @@ public class feuille_pointageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.feuille_de_pointage);
+
+        Button boutonRetour = (Button) findViewById(R.id.button38);
+        boutonRetour.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
 
     }
 }
